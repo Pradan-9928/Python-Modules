1ab86dce7e840c9f5bfe6d39241ae8a8b1d10775
Pradan-9928 <rpradan9928@gmail.com>
2026-09-22 22:00:13 +0530

    September 22nd

--- Pandas_Tutorial.py
+++ Pandas_Tutorial.py
@@ -22,3 +22,37 @@ print(new_series)
 #You can change the index of a value by the next attribute
 
 print(new_series.loc['a'])
+
+#To find the specific value of any data you can acess it by specifying its index values sort of like dictionary
+
+print(new_series)
+new_series.loc['c']=20
+print(new_series)
+
+#Output
+# a    100
+# b    102
+# c    104
+# dtype: int64
+# a    100
+# b    102
+# c     20
+# dtype: int64
+
+#To change the value of the given index of a series by loc method
+print(new_series)
+print(new_series.iloc[1])
+print(new_series.iloc[0])
+print(new_series.iloc[2])
+
+# Output
+# a    100
+# b    102
+# c     20
+# dtype: int64
+# 102
+# 100
+# 20
+#You can print the values of the series bu also using the original and primitve index values of the numbers
+
+print(series[series>=100])
