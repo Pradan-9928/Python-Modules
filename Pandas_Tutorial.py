6ed45d407788d6c758106f3ee9c91a8321167f07
Pradan-9928 <rpradan9928@gmail.com>
2026-09-24 20:51:14 +0530

    September 24TH

--- Pandas_Tutorial.py
+++ Pandas_Tutorial.py
@@ -19,7 +19,7 @@ print(series)
 new_series=pd.Series(data,index=['a','b','c'])
 print(new_series)
 
-#You can change the index of a value by the next attribute
+#You can change the index of a value by the adding it in an attribute
 
 print(new_series.loc['a'])
 
@@ -54,4 +54,15 @@ print(new_series.iloc[2])
 # 100
 # 20
 #You can print the values of the series bu also using the original and primitve index values of the numbers
+
+data=[0,101,203,190,700,604]
+series=pd.Series(data)
+print(series[series>500])
+
+# the above example is one of the kind to filter out and display values of the speicific condition
+
+calories={"pizza":200,"burger":300}
+dict_series=pd.Series(calories)
+dict_series.loc["burger"]+=100
+print(dict_series.loc["burger"])
 
